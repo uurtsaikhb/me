d523b36b5f991ece3bca556ae82da38fedee5799
Uurtsaikh <ubatbile@amazon.com>
2018-04-03 12:35:44 -0700

    about me and experience page is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/about',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutMeComponent
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'education',
+    component: EducationComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    EducationComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {}
